a0fe09d8516db46da9d963697c5a73805de8f757
jzt27 <jivanthiru@gmail.com>
2026-04-20 19:04:34 +0100

    Add new docs

--- sidebars.js
+++ sidebars.js
@@ -1,90 +1,30 @@
 const sidebars = {
   tutorialSidebar: [
-    {
-      type: 'doc',
-      id: '10-minute-chart-checklist',
-      label: '✅ 10-minute chart checklist',
-    },
-    {
-      type: 'doc',
-      id: 'embers-core-principles',
-      label: '📍 Ember’s core principles',
-    },
-    {
-      type: 'doc',
-      id: 'data-viz-learning-journey',
-      label: '↗️ Data viz learning journey',
-    },
-    {
-      type: 'doc',
-      id: 'ai-agents-for-data-viz',
-      label: '🤖 [TBC] AI agents for data viz',
-    },
-    {
-      type: 'doc',
-      id: 'roles-responsibilities',
-      label: '👥 Roles, responsibilities and...',
-    },
-    {
-      type: 'doc',
-      id: 'chart-standards-in-detail',
-      label: '📊 Chart standards in detail',
-    },
-    {
-      type: 'doc',
-      id: 'choosing-the-right-chart',
-      label: '🤔 Choosing the right chart',
-    },
+    'intro',
+    'roles-responsibilities',
+    'ember-core-principles',
+    'data-viz-learning-journey',
+    'choosing-the-right-chart',
+    'chart-standards-in-detail',
+    '10-minute-chart-checklist',
+    'ai-agents-for-dataviz',
     {
       type: 'category',
-      label: '⚙️ How to: Data viz tools',
-      collapsible: true,
-      collapsed: false,
+      label: 'Tools',
       items: [
-        {
-          type: 'doc',
-          id: 'tools/flourish',
-          label: 'Flourish',
-        },
-        {
-          type: 'doc',
-          id: 'tools/r',
-          label: 'R',
-        },
-        {
-          type: 'doc',
-          id: 'tools/affinity-advanced',
-          label: 'Affinity (advanced)',
-        },
+        'tools/affinity-advanced',
+        'tools/flourish',
+        'tools/r',
       ],
     },
     {
       type: 'category',
-      label: '🚀 Advanced visual storytelling',
-      collapsible: true,
-      collapsed: false,
+      label: 'Advanced',
       items: [
-        {
-          type: 'doc',
-          id: 'advanced/maps',
-          label: 'Maps',
-        },
-        {
-          type: 'doc',
-          id: 'advanced/project-examples',
-          label: 'Project examples and...',
-        },
-        {
-          type: 'doc',
-          id: 'advanced/training-resources',
-          label: 'Training and resources',
-        },
+        'advanced/maps',
+        'advanced/project-examples',
+        'advanced/training-resources',
       ],
-    },
-    {
-      type: 'doc',
-      id: 'tasks-for-this-section',
-      label: '🧾 [To delete] Tasks for this...',
     },
   ],
 };
